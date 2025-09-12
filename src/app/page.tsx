66cb4a1b07a8690d2fdec0384dce392f4210c078
Admin D <danan12453@gmail.com>
2025-09-12 19:31:18 +0700

    revamp the landing page to use weekly schedule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import LandingNav from "@/components/landing-nav";
 import LandingHero from "@/components/landing-hero";
+import ScheduleList from "@/components/schedule-list";
+import { schedules } from "./app/data";
 
 export default function Home() {
 	return (
 		<div className="min-h-screen">
 			<LandingNav />
 			<LandingHero />
+			<div className="px-4 gap-4 lg:px-6 flex flex-col">
+				<ScheduleList schedules={schedules} />
+			</div>
 		</div>
 	);
 }
--- src/components/schedule-list.tsx
+++ src/components/schedule-list.tsx
@@ -1,5 +1,5 @@
-import { RequestDialog } from "./request-dialog"
-import ScheduleCard from "./schedule-card"
+import { RequestDialog } from "./request-dialog";
+import ScheduleCard from "./schedule-card";
 import { ScheduleProps } from "./type";
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
@@ -11,7 +11,7 @@ export default function ScheduleList({
 	return (
 		<div>
 			<div className="flex justify-between items-center mb-4">
-				<h1 className="text-2xl font-bold">Weekly Schedule</h1>
+				<h1 className="text-2xl font-bold">This Week's Schedule</h1>
 				{/* <Button className="bg-blue-400">Request Lab +</Button> */}
 				<RequestDialog />
 			</div>
