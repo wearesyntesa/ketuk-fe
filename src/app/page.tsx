ac0caa553c57e5a1d7bf1ee9199abd901d37565f
alfazh123 <alfazh291@gmail.com>
2025-09-28 16:17:19 +0700

    fix component on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import LandingNav from "@/components/landing-nav";
 import LandingHero from "@/components/landing-hero";
-import ScheduleList from "@/components/schedule-list";
 import { schedules } from "./app/data";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import ScheduleCard from "@/components/schedule-card";
 
 export default function Home() {
 	return (
@@ -9,7 +10,14 @@ export default function Home() {
 			<LandingNav />
 			<LandingHero />
 			<div className="px-4 gap-4 lg:px-6 flex flex-col">
-				<ScheduleList schedules={schedules} />
+				<ScrollArea className="rounded-lg w-full">
+					<div className="flex gap-4 w-full">
+						{schedules.map((schedule, index) => (
+							<ScheduleCard key={index} {...schedule} />
+						))}
+					</div>
+					<ScrollBar orientation="horizontal" />
+				</ScrollArea>
 			</div>
 		</div>
 	);
