b6e75912c248e232111a786a5484bd3b21820ba0
alfazh123 <alfazh291@gmail.com>
2025-12-01 13:58:43 +0700

    add configuration permission image domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ["lh3.googleusercontent.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+				port: "",
+			},
+		],
 	},
 };
 
