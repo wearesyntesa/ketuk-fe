422c8f8cf7798b6a7c9f43be8616a3803ce7986f
alfazh123 <alfazh291@gmail.com>
2025-12-12 00:07:18 +0700

    initiate unblock page

--- src/components/type.ts
+++ src/components/type.ts
@@ -99,3 +99,111 @@ export interface AllUser {
 	updated_at: string;
 }
 
+export interface ItemDialogProps {
+	categoryId: number;
+	name: string;
+	note: string;
+	condition: "Baik" | "Good" | "Poor";
+	year?: number;
+}
+
+export interface Ticket {
+	id: number;
+	userId: number;
+	user: UserType;
+	title: string;
+	description: string;
+	status: "pending" | "approved" | "rejected" | string;
+	createdAt: string;
+	updatedAt: string;
+	reason: string;
+}
+
+export interface UnblockingForm {
+	endDate: Date | undefined;
+	startDate: Date | undefined;
+	semester: "Ganjil" | "Genap" | string;
+	tahun: number;
+	userId: number;
+}
+
+export interface UnblockingResponse {
+	semester: "Ganjil" | "Genap" | string;
+	startDate: string;
+	endDate: string;
+	tahun: number;
+	userId: number;
+	id: number;
+}
+
+export interface AuditLogTicket {
+	id: number;
+	ticketId: number;
+	userId: number;
+	action: string;
+	newValue: string;
+	oldValue?: string;
+	changes?: string;
+	createdAt: string;
+	user: UserType;
+}
+
+export interface AuditLogUser {
+	id: number;
+	userId: number;
+	action: string;
+	newValue: string;
+	oldValue?: string;
+	changes?: string;
+	createdAt: string;
+	user: UserType;
+	title: string;
+	description: string;
+	status: string;
+	reason: string;
+	createdAtTicket: string;
+	updatedAtTicket?: string;
+	approvedAt?: string;
+}
+
+export interface ScheduleTicket {
+	idSchedule: number;
+	title: string;
+	startDate: string;
+	endDate: string;
+	userId: number;
+	kategori: string;
+	description: string;
+	createdAt: string;
+	updatedAt: string;
+	user: UserType;
+	tickets?: Ticket[];
+}
+
+export interface ScheduleReguler {
+	idSchedule: number;
+	title: string;
+	startDate: string;
+	endDate: string;
+	userId: number;
+	kategori: string;
+	description: string;
+	createdAt: string;
+	updatedAt: string;
+	user: UserType;
+}
+
+export interface MergeSchedultType {
+	idSchedule: number;
+	title: string;
+	startDate: string;
+	endDate: string;
+	userId: number;
+	kategori: string;
+	description: string;
+	createdAt: string;
+	updatedAt: string;
+	user: UserType;
+	tickets?: Ticket[];
+	isReguler: boolean;
+}
