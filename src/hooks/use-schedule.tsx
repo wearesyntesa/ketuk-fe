1af28f57aa4511af0be4310a5917d57d2eee2189
alfazh123 <alfazh291@gmail.com>
2025-12-15 17:38:49 +0700

    add some hooks

--- src/hooks/use-schedule.tsx
+++ src/hooks/use-schedule.tsx
@@ -6,67 +6,96 @@ import { useState } from "react";
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://192.168.10.184:8081";
 
 export const useSchedule = (token: string) => {
-    const [schedules, setSchedules] = useState<MergeSchedultType[]>([]);
-    const [ticketSchedules, setTicketSchedules] = useState<ScheduleTicket[]>([]);
-    const [regulerSchedules, setRegulerSchedules] = useState<ScheduleReguler[]>([]);
+	const [schedules, setSchedules] = useState<MergeSchedultType[]>([]);
+	const [ticketSchedules, setTicketSchedules] = useState<ScheduleTicket[]>([]);
+	const [regulerSchedules, setRegulerSchedules] = useState<ScheduleReguler[]>(
+		[]
+	);
 
-    const handleGetAllTicketSchedules = async () => {
-        try {
-            const response = await fetch(`${API_URL}/api/schedules/tickets/v1`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "authorization": `Bearer ${token}`,
-                },
-            });
-            const data = await response.json();
-            
-            if (data.success) {
-                console.log("data schedule ticket", data.data);
-                setTicketSchedules(data.data);
-            } else {
-                console.error("Failed to fetch ticket schedules:", data.message);
-            }
-        } catch (err) {
-            console.error("Fetch ticket schedules error:", err);
-        } finally {
-            console.log("Fetch ticket schedules completed");
-        }
-    }
+	const handleGetAllTicketSchedules = async () => {
+		try {
+			const response = await fetch(`${API_URL}/api/schedules/tickets/v1`, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			const data = await response.json();
 
-    const handleGetAllRegulerSchedules = async () => {
-        try {
-            const response = await fetch(`${API_URL}/api/schedules/reguler/v1`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json", 
-                    "authorization": `Bearer ${token}`,
-                },
-            });
-            const data = await response.json();
-            if (data.success) {
-                setRegulerSchedules(data.data);
-            } else {
-                console.error("Failed to fetch reguler schedules:", data.message);
-            }
-        } catch (err) {
-            console.error("Fetch reguler schedules error:", err);
-        } finally {
-            console.log("Fetch reguler schedules completed");
-        }
-    }
+			if (data.success) {
+				const dataRes: ScheduleTicket[] = data.data;
+				setTicketSchedules(
+					dataRes.map((item: ScheduleTicket) => ({
+						idSchedule: item.idSchedule,
+						title: item.title,
+						startDate: item.startDate,
+						endDate: item.endDate,
+						userId: item.userId,
+						kategori: item.kategori,
+						description: item.description,
+						createdAt: item.createdAt,
+						updatedAt: item.updatedAt,
+						user: item.user,
+						tickets: item.tickets,
+					}))
+				);
+				return dataRes;
+			} else {
+				console.error("Failed to fetch ticket schedules:", data.message);
+			}
+		} catch (err) {
+			console.error("Fetch ticket schedules error:", err);
+		} finally {
+			console.log("Fetch ticket schedules completed");
+		}
+	};
 
-    const handleGetAllSchedules = async () => {
-        await handleGetAllTicketSchedules();
-        await handleGetAllRegulerSchedules();
-        console.log("ticketSchedules", ticketSchedules);
-        console.log("regulerSchedules", regulerSchedules);
-    }
+	const handleGetAllRegulerSchedules = async () => {
+		try {
+			const response = await fetch(`${API_URL}/api/schedules/reguler/v1`, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			const data = await response.json();
+			if (data.success) {
+				const dataRes: ScheduleReguler[] = data.data;
+				setRegulerSchedules(
+					dataRes.map((item: ScheduleReguler) => ({
+						idSchedule: item.idSchedule,
+						title: item.title,
+						startDate: item.startDate,
+						endDate: item.endDate,
+						userId: item.userId,
+						kategori: item.kategori,
+						description: item.description,
+						createdAt: item.createdAt,
+						updatedAt: item.updatedAt,
+						user: item.user,
+					}))
+				);
+				return data.data;
+			} else {
+				console.error("Failed to fetch reguler schedules:", data.message);
+			}
+		} catch (err) {
+			console.error("Fetch reguler schedules error:", err);
+		} finally {
+			console.log("Fetch reguler schedules completed");
+		}
+	};
 
-    return {
-        schedules,
-        ticketSchedules,
-        regulerSchedules,
-        handleGetAllSchedules,
-    }
-}
+	return {
+		schedules,
+		ticketSchedules,
+		regulerSchedules,
+		setSchedules,
+		setTicketSchedules,
+		setRegulerSchedules,
+		handleGetAllRegulerSchedules,
+		handleGetAllTicketSchedules,
+	};
+};
--- src/hooks/use-unblocking.tsx
+++ src/hooks/use-unblocking.tsx
@@ -8,84 +8,87 @@ export const useUnblocking = () => {
     const [dataUnblocking, setDataUnblocking] = useState<UnblockingResponse[]>([] as UnblockingResponse[]);
 
     const handleGetUnblocking = async (token: string) => {
-        try {
-            const response = await fetch(`${API_URL}/api/unblockings/v1`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "authorization": `Bearer ${token}`,
-                },
-            }); 
-            const data = await response.json();
-            if (data.success) {
-                if (dataUnblocking) {
-                    setDataUnblocking(data.unblockings);
-                    setUnblocking(true);
-                } else {
-                    setUnblocking(false);
-                }
-            } else {
-                console.error("Failed to fetch unblocking:", data.message);
-            }
-        } catch (err) {
-            console.error("Fetch unblocking error:", err);
-        } finally {
-            console.log("Fetch unblocking completed");
-        }
-    }
+			try {
+				const response = await fetch(`${API_URL}/api/unblockings/v1`, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				});
+				const data = await response.json();
+				if (data.success) {
+					if (dataUnblocking) {
+						setDataUnblocking(data.unblockings);
+						setUnblocking(true);
+					} else {
+						setUnblocking(false);
+					}
+				} else {
+					console.error("Failed to fetch unblocking:", data.message);
+				}
+			} catch (err) {
+				console.error("Fetch unblocking error:", err);
+			} finally {
+				console.log("Fetch unblocking completed");
+			}
+		};
 
-    const handleGetUnblockingById = async (token: string, id: number) => {
-        try {
-            const response = await fetch(`${API_URL}/api/unblockings/v1/${id}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "authorization": `Bearer ${token}`,
-                },
-            });
-            const data = await response.json();
-            if (data.success) {
-                return data.unblocking;
-            } else {
-                console.error("Failed to fetch unblocking by ID:", data.message);
-            }
-        } catch (err) {
-            console.error("Fetch unblocking by ID error:", err);
-        }
-    }
+		const handleGetUnblockingById = async (token: string, id: number) => {
+			try {
+				const response = await fetch(`${API_URL}/api/unblockings/v1/${id}`, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				});
+				const data = await response.json();
+				if (data.success) {
+					return data.unblocking;
+				} else {
+					console.error("Failed to fetch unblocking by ID:", data.message);
+				}
+			} catch (err) {
+				console.error("Fetch unblocking by ID error:", err);
+			}
+		};
 
-    const handlePostUnblocing = async (token: string, unblockingData: UnblockingForm) => {
-        try {
-            const response = await fetch(`${API_URL}/api/unblockings/v1`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "authorization": `Bearer ${token}`,
-                },
-                body: JSON.stringify({
-                    endDate: unblockingData.endDate,
-                    startDate: unblockingData.startDate,
-                    semester: unblockingData.semester,
-                    tahun: unblockingData.tahun,
-                    userId: unblockingData.userId,
-                }),
-            });
+		const handlePostUnblocing = async (
+			token: string,
+			unblockingData: UnblockingForm
+		) => {
+			try {
+				const response = await fetch(`${API_URL}/api/unblockings/v1`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+					body: JSON.stringify({
+						endDate: unblockingData.endDate,
+						startDate: unblockingData.startDate,
+						semester: unblockingData.semester,
+						tahun: unblockingData.tahun,
+						userId: unblockingData.userId,
+					}),
+				});
 
-            const data = await response.json();
+				const data = await response.json();
 
-            if (data.success) {
-                setUnblocking(true);
-                window.location.reload();
-                return data;
-            } else {
-                console.error("Failed to post unblocking:", data.message);
-            }
-        } catch (err) {
-            console.error("Post unblocking error:", err);
-        } finally {
-            console.log("Post unblocking completed");
-        }
-    }
+				if (data.success) {
+					setUnblocking(true);
+					window.location.reload();
+					return data;
+				} else {
+					console.error("Failed to post unblocking:", data.message);
+				}
+			} catch (err) {
+				console.error("Post unblocking error:", err);
+			} finally {
+				console.log("Post unblocking completed");
+			}
+		};
 
     return {
         unblocking,
